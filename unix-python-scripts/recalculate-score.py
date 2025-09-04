b858c7de02fb0412b5b2afea79ffe1605f16983b
ddlbio25 <ledan7278@gmail.com>
2025-09-04 09:25:14 -0400

    added .close() to some python scripts

--- unix-python-scripts/recalculate-score.py
+++ unix-python-scripts/recalculate-score.py
@@ -9,10 +9,13 @@ for line in file:
     #line[4] = orig score
     # line[2] - line[1] = feature size = chrom_end - chrom_start
     new_score = float(line[4]) * (int(line[2]) - int(line[1]))
+    if line[5] == "-":
+        new_score = new_score * -1
     line[4] = str(new_score)
     # create new tab delimited line and append to list
     new_line = "\t".join(line)
     new_scores.append(new_line)
+file.close()
 
 #write new bed file
 
@@ -23,5 +26,6 @@ for i in range(len(new_scores)):
         output.write(f"{new_scores[i]}") 
     else:
         output.write(f"{new_scores[i]}\n")
+output.close()
 
  
