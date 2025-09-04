4bb2ac05ef226848a8c0c3b03c8d0c0b4b71b0b5
ddlbio25 <ledan7278@gmail.com>
2025-09-04 13:53:01 -0400

    fixed gtf2bed.py to actually parse gene name from attribute

--- unix-python-scripts/gtf2bed.py
+++ unix-python-scripts/gtf2bed.py
@@ -5,16 +5,22 @@ file = open("/Users/cmdb/Data/References/hg38/gencode.v46.basic.annotation.gtf")
 for line in file:
     if line.startswith("#"):
         continue
-    line = line.strip("\n").split("\t")
+    line = line.rstrip("\n").split("\t")
     if line[2] != "gene":
         continue
     line[3] = str(int(line[3]) - 1) #subtracts 1 from start position
-    gene_name = line[8]
+    attribute = line[8]
+    
+    #split attribute by ": ", then parse gene name
+    gene_name = attribute.rstrip(";").split("; ")
+    gene_name = gene_name[2]
+    gene_name = gene_name.split(" ")[1]
+
     chr = line[0]
     start = line[3]
     end = line[4]
     output = [chr, start, end, gene_name]
 
-    print("\t".join(output))    
+    print("\t".join(output)) 
 
 file.close()
