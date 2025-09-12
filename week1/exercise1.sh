ee81034c84d9d20d42bd64967a60b28861ebee94
ddlbio25 <ledan7278@gmail.com>
2025-09-12 12:06:35 -0400

    first exercise

---
+++ week1/exercise1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Get hg19 chromosome sizes
+wget https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.chrom.sizes
+
+#remove unmmaped contigs and rename chrM to chrMT
+grep -v _ hg19.chrom.sizes | sed 's/M/MT/' > hg19-main.chrom.sizes
+
+# make 1 Mb windows
+
+bedtools makewindows -g hg19_main.chrom.sizes -w 1000000 > hg19-1mb.bed
+wc -l hg19-1mb.bed
+#     3114 hg19-1mb.bed
+
+# create hg19-kc.bed with one transcript per gene
+mv ~/Downloads/hg19-kc.tsv .
+wc -l hg19-kc.tsv
+#   80270 hg19-kc.tsv
+cut -f1-3,5 hg19-kc.tsv > hg19-kc.bed
+head hg19-kc.bed
+
+#count how many genes are in each interval
+
+# -c flag counts the number of hits for each window in -a that intersect the transcripts in -b
+bedtools intersect -c -a hg19_1mb.bed -b hg19_kc.bed > hg19-kc-count.bed
