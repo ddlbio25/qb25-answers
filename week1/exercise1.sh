74e75e689fae93cb685e39718af12bf02b573b7b
ddlbio25 <ledan7278@gmail.com>
2025-09-18 22:45:07 -0400

    fixed exercise 1 and added files i forgot

--- week1/exercise1.sh
+++ week1/exercise1.sh
@@ -3,12 +3,15 @@
 # Get hg19 chromosome sizes
 wget https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.chrom.sizes
 
+#less
+less hg19.chrom.sizes
+
 #remove unmmaped contigs and rename chrM to chrMT
 grep -v _ hg19.chrom.sizes | sed 's/M/MT/' > hg19-main.chrom.sizes
 
 # make 1 Mb windows
 
-bedtools makewindows -g hg19_main.chrom.sizes -w 1000000 > hg19-1mb.bed
+bedtools makewindows -g hg19-main.chrom.sizes -w 1000000 > hg19-1mb.bed
 wc -l hg19-1mb.bed
 #     3114 hg19-1mb.bed
 
@@ -23,3 +26,5 @@ head hg19-kc.bed
 
 # -c flag counts the number of hits for each window in -a that intersect the transcripts in -b
 bedtools intersect -c -a hg19_1mb.bed -b hg19_kc.bed > hg19-kc-count.bed
+wc -l hg19-kc-count.bed
+# 3114 hg19-kc-count.bed
